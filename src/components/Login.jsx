21e0a5cbadd230326786f5ee7b5976d7fac90685
Sofiia Bernatska <128867240+sofiabern@users.noreply.github.com>
2024-11-24 03:50:35 +0200

    validation on submit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,7 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 export default function Login() {
+const [emailIsInvalid, setEmailIsInvalid] = useState(false)
+
 const email = useRef()
 const password = useRef()
 
@@ -9,10 +11,16 @@ const password = useRef()
     const enteredEmail = email.current.value
     const enteredPassword = password.current.value
 
-    console.log(enteredEmail, enteredPassword)
+const emailIsValid = enteredEmail.includes('@')
 
-    email.current.value=''
-    password.current.value=''
+if(!emailIsValid){
+  setEmailIsInvalid(true)
+  return
+}
+
+setEmailIsInvalid(false)
+
+console.log('Sending HTTP request...')
   }
 
  
@@ -24,6 +32,7 @@ const password = useRef()
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email"  ref={email}/>
+          <div className="control-error">{emailIsInvalid && <p>Please enter a valid email address.</p>}</div>
         </div>
 
         <div className="control no-margin">
